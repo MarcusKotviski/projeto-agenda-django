bd83e1bccc84c028b40f11140740d1c955863bcf
Marcus <marcuskotviski@gmail.com>
2023-10-23 10:51:19 -0300

    Criando a pagina de contato

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,3 +10,13 @@ def index(request):
     }
 
     return render(request,'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.get(pk=contact_id)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(request,'contact/contact.html', context)
