4e04a96b65f8aee1f6cf9c94bebd1fcd2eda993a
Marcus <marcuskotviski@gmail.com>
2023-10-24 11:29:22 -0300

    Adicionando cabeçalho

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,6 +6,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(request,'contact/index.html', context)
@@ -16,8 +17,12 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title
+
     }
 
     return render(request,'contact/contact.html', context)
