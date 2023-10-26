7c3607ff6449d38355f83120b8b8d17c00217b37
Marcus <marcuskotviski@gmail.com>
2023-10-26 10:48:01 -0300

    Criando formulario de criação

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,17 +1,32 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
-
+from django import forms
 from contact.models import Contact
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone'
+
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
     
     context = {
-    
-    }
+                'form': ContactForm()
+            }
 
     return render(
-        request,
-        'contact/create.html',
-        context
-    )
+            request,
+            'contact/create.html',
+            context
+        )
