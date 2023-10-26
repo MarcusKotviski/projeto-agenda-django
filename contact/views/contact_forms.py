a87c5058375ead7d3e4a00928e237ede19be9fb5
Marcus <marcuskotviski@gmail.com>
2023-10-26 11:51:36 -0300

    Organizando os forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,6 @@
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
-from django import forms
-from contact.models import Contact
+from django.shortcuts import render
+from contact.forms import ContactForm
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone'
 
 def create(request):
     if request.method == 'POST':
