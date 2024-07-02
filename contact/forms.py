ead5c4d4eebedbd6fe2b156e0d78b5288bafee16
Marcus <marcuskotviski@gmail.com>
2024-07-02 08:14:44 -0300

    Adicionando opção de fotos para os contatos

--- contact/forms.py
+++ contact/forms.py
@@ -1,42 +1,22 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from . import models
+from django.contrib.auth.forms import UserCreationForm
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+
+
+    picture = forms.ImageField(
+        widget=forms.FileInput(attrs={'accpet': 'image/*'})
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
 
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
-            'email', 'description', 'category',
+            'email', 'description', 'category', 'picture'
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -68,3 +48,6 @@ class ContactForm(forms.ModelForm):
 
         return first_name
     
+
+class RegisterForm(UserCreationForm):
+    ...
