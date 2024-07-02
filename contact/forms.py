d9cfa1d257d7d62e3b59aabd636871753fe00d33
Marcus <marcuskotviski@gmail.com>
2024-07-02 18:28:29 -0300

    Adicionando validação aos campos de cadastro do usuario

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from . import models
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -50,4 +51,21 @@ class ContactForm(forms.ModelForm):
     
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(required=True, min_length=3)
+    last_name = forms.CharField(required=True, min_length=3)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists:
+            self.add_error('email', ValidationError('Este e-mail ja foi cadastrado.', code='invalid'))
+
+        return email
